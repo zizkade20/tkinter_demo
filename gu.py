9f805ccf6b5e95a7b5ccc3b76fcef92d4965c378
zizkade20 <zizkade20@sps-prosek.cz>
2022-03-01 13:59:42 +0100

    hodina začala

--- gu.py
+++ gu.py
@@ -3,7 +3,7 @@ import tkinter as tk
 window = tk.Tk()
 window.geometry("250x250")
 window.title("Aplikace v1.0 beta")
-label1 = tk.Label(text="Ahoj!      :) ")
+label1 = tk.Label(text="Hodina začala")
 label1.pack()
-# 4
+
 window.mainloop()
