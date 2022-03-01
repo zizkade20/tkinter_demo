f3b031d84066f9ab8e82e9484a789efe052e817b
zizkade20 <zizkade20@sps-prosek.cz>
2022-03-01 15:41:11 +0100

    entry

--- calc.py
+++ calc.py
@@ -1,8 +1,15 @@
 import tkinter as tk
 
+#def calc_get():
+
 window = tk.Tk()
 window.geometry("400x420")
 window.title("Fortnite")
+
+entry = tk.Entry(
+    width=40,
+
+)
 
 button1 = tk.Button(
     text="1",
@@ -116,22 +123,22 @@ buttondelete = tk.Button(
     width=10,
     height=5,
 )
-
-button1.grid(row=0, column=0)
-button2.grid(row=0, column=1)
-button3.grid(row=0, column=2)
-button4.grid(row=1, column=0)
-button5.grid(row=1, column=1)
-button6.grid(row=1, column=2)
-button7.grid(row=2, column=0)
-button8.grid(row=2, column=1)
-button9.grid(row=2, column=2)
-button0.grid(row=3, column=1)
-buttonplus.grid(row=0, column=3)
-buttonminus.grid(row=1, column=3)
-buttonkrat.grid(row=2, column=3)
-buttondeleno.grid(row=3, column=3)
-buttonrovnase.grid(row=3, column=2)
-buttondelete.grid(row=3, column=0)
+entry.grid(columnspan=4)
+button1.grid(row=1, column=0)
+button2.grid(row=1, column=1)
+button3.grid(row=1, column=2)
+button4.grid(row=2, column=0)
+button5.grid(row=2, column=1)
+button6.grid(row=2, column=2)
+button7.grid(row=3, column=0)
+button8.grid(row=3, column=1)
+button9.grid(row=3, column=2)
+button0.grid(row=4, column=1)
+buttonplus.grid(row=1, column=3)
+buttonminus.grid(row=2, column=3)
+buttonkrat.grid(row=3, column=3)
+buttondeleno.grid(row=4, column=3)
+buttonrovnase.grid(row=4, column=2)
+buttondelete.grid(row=4, column=0)
 
 window.mainloop()
